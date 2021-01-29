55e6a80c07146972c4b58c27481d4bdeae87818d
AkintoyeOlamide <akintoyeolamide500@gmail.com>
2021-01-29 10:41:02 +0100

    finishing styling project

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,6 +1,6 @@
 module CategoriesHelper
   def category_main_image(category)
-    if category.articles.last.image.attached?
+    if category.articles && category&.articles&.last&.image&.attached?
       link_to(image_tag(category.articles.last.image), category_path(category))
     else
       link_to(image_tag('article.png'), category_path(category))
