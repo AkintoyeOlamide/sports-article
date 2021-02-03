43d1c1ca01d5bfa80e25bae0267ea88c72428c5c
AkintoyeOlamide <akintoyeolamide500@gmail.com>
2021-02-03 18:02:08 +0100

    fixing Rubocop errors in the models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,10 +3,4 @@ class Comment < ApplicationRecord
                                                 too_long: '200 characters in comment is the maximum allowed.' }
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :article, class_name: 'Article', foreign_key: 'article_id'
-
-  if comment.user.profile_image.attached? 
-  image_tag(comment.user&.profile_image) 
-  else 
-  <img src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png">
-  end 
 end
