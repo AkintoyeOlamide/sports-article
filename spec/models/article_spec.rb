fd086aa8f9cf594740ab4dd214b5923192cc2f8f
AkintoyeOlamide <akintoyeolamide500@gmail.com>
2021-02-03 22:28:12 +0100

    added negative test cases to the models

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -11,4 +11,24 @@ RSpec.describe Article, type: :model do
     it { should belong_to(:user) }
     it { should have_many(:comments) }
   end
+
+  it 'Article is not valid without title' do
+    @article = Article.new(body: 'b', category_id: category.id, user_id: user.id)
+    expect(@article).to_not be_valid
+  end
+
+  it 'Article is not valid without body' do
+    @article = Article.new(title: 'a', category_id: category.id, user_id: user.id)
+    expect(@article).to_not be_valid
+  end
+
+  it 'Article is not valid without user' do
+    @article = Article.new(title: 'a', body: 'b', category_id: category.id)
+    expect(@article).to_not be_valid
+  end
+
+  it 'Article is not valid without category' do
+    @article = Article.new(title: 'a', body: 'b', user_id: user.id)
+    expect(@article).to_not be_valid
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe User, type: :model do
     it { should have_many(:articles) }
     it { should have_many(:comments) }
   end
+
+  
+  it 'User is not valid without name attribute' do
+    user = User.new(name: nil)
+    expect(user).to_not be_valid
+  end
+
+  it 'User has many articles' do
+    expect(User.reflect_on_association(:articles).macro).to eq(:has_many)
+  end
+
+  it 'User has many votes' do
+    expect(User.reflect_on_association(:articles).macro).to eq(:has_many)
+  end
 end
